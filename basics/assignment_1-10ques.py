63109894dba2e628217a7da47e07378d67152b97
San120806 <saniyaakapure@gmail.com>
2025-01-13 15:14:28 +0530

    Add files via upload

---
+++ basics/assignment_1-10ques.py
@@ -0,0 +1,77 @@
+# #Print “Hello, World!”
+# print("Hello, World!")
+
+##Variables and Data Types - Prompt the user for their name and age.
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# print(f"Your name is {name} and your age is {age}.")
+
+# #arithmetic operators
+# num_1= int(input("enter the 1st no.: "))
+# num_2= int(input("enter the 2nd no.: "))
+# print(f"sum is: {num_1+num_2}")
+# print(f"Difference is : {num_1 - num_2}")
+# print(f"Product is : {num_1 * num_2}")
+# print(f"Division is : {num_1 / num_2}")
+# print(f"Modulus is : {num_1 % num_2}")
+
+# celsius to Faherenheit conversion
+# cel = float(input("Enter temperature in Celsius: "))
+# fahrnt = (cel * 9/5) + 32
+# print(f"The temperature in Fahrenheit is {fahrnt}")
+
+# #swap 2no.s
+# a = input("Enter the first variable: ")
+# b = input("Enter the second variable: ")
+# print(f"Before swapping: a = {a}, b = {b}")
+# temp = a
+# a = b
+# b = temp
+# print(f"After swapping: a = {a}, b = {b}")
+
+# even or odd
+# a=int(input("enter the no.:"))
+# print(f"the no. is:{a} ")
+# if a%2==0:
+#     print(f"the no. is even: {a}")
+# else:
+#     print(f"the no. is odd: {a}")
+
+
+# # vowel v/s consonant
+# letter = input("Enter a letter: ")
+# if letter in 'aeiou':
+#     print(f"{letter} is a vowel.")
+# elif letter.isalpha():
+#     print(f"{letter} is a consonant.")
+# else:
+#     print("Invalid input. Please enter a single letter.")
+
+# # square , cube , square root
+# number = float(input("Enter a number: "))
+# print(f"Square: {number ** 2}")
+# print(f"Cube: {number ** 3}")
+# print(f"Square Root: {number ** 0.5}")
+
+# # area of circle
+# radius = float(input("Enter the radius of the circle: "))
+# pi = 3.142
+# area = pi * (radius ** 2)
+# print(f"The area of the circle is {area}")
+
+# # Simple Interest Calculation
+# princi = float(input("Enter the principal amount: "))
+# rate = float(input("Enter the rate of interest: "))
+# time = float(input("Enter the time in years: "))
+# simpleinterest = (princi * rate * time) / 100
+# print(f"The simple interest is {simpleinterest:.2f}")
+
+
+
+
+
+
+
+
+
+
